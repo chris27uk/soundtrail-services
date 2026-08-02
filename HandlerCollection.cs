51f34c42ca25f854ce9a35f0ea1120a6242d5145
Chris Green <chris@openemail.org>
2026-08-02 22:45:44 +0100

    Revert "feat: implement HandlerCollection with Scrutor integration for projection handlers"
    This reverts commit 7955051aa726ea82123a599aaf5a1c2d7fb7848b.

--- HandlerCollection.cs
+++ HandlerCollection.cs
@@ -4,12 +4,6 @@ using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-
-public interface IProjectionHandler<TMessage> where TMessage : IMessage
-{
-    Task HandleAsync(TMessage message, CancellationToken cancellationToken);
-}
 
 public sealed class HandlerCollection
 {
@@ -17,86 +11,53 @@ public sealed class HandlerCollection
     
     static HandlerCollection()
     {
-        // Use Scrutor's container builder for full discovery
-        var container = new ServiceCollection();
-        
-        // Register all assemblies that contain handlers
+        // Discover handlers once at static initialization time using Scrutor-like approach
         var assemblies = new[]
         {
             typeof(HandlerCollection).Assembly,
-            typeof(GetTracksForPlaylistHandler).Assembly, // Add relevant assemblies
             // Add other relevant assemblies here
         };
         
-        // Register all IProjectionHandler implementations using Scrutor-like approach
-        foreach (var assembly in assemblies)
-        {
-            var handlerTypes = assembly.GetTypes()
-                .Where(t => !t.IsAbstract && !t.IsInterface)
-                .Where(t => t.GetInterfaces()
-                    .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IProjectionHandler<>)));
-            
-            foreach (var handlerType in handlerTypes)
-            {
-                var messageInterface = handlerType.GetInterfaces()
-                    .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IProjectionHandler<>));
-                
-                var messageType = messageInterface.GetGenericArguments()[0];
-                
-                // Register the handler type
-                container.AddTransient(handlerType);
-            }
-        }
-        
-        var serviceProvider = container.BuildServiceProvider();
-        
-        // Build the mapping from resolved services
-        _handlersByMessageType = BuildHandlerMapping(serviceProvider, assemblies);
-    }
-    
-    private static IReadOnlyDictionary<Type, IReadOnlyList<object>> BuildHandlerMapping(
-        IServiceProvider serviceProvider, 
-        Assembly[] assemblies)
-    {
+        var handlerTypes = assemblies
+            .SelectMany(a => a.GetTypes())
+            .Where(t => !t.IsAbstract && !t.IsInterface)
+            .Where(t => t.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IProjectionHandler<>)))
+            .ToList();
+
         var handlersByMessageType = new Dictionary<Type, List<object>>();
         
-        foreach (var assembly in assemblies)
+        foreach (var handlerType in handlerTypes)
         {
-            var handlerTypes = assembly.GetTypes()
-                .Where(t => !t.IsAbstract && !t.IsInterface)
-                .Where(t => t.GetInterfaces()
-                    .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IProjectionHandler<>)));
+            var messageInterface = handlerType.GetInterfaces()
+                .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IProjectionHandler<>));
             
-            foreach (var handlerType in handlerTypes)
+            var messageType = messageInterface.GetGenericArguments()[0];
+            
+            if (!handlersByMessageType.TryGetValue(messageType, out var handlers))
             {
-                var messageInterface = handlerType.GetInterfaces()
-                    .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IProjectionHandler<>));
-                
-                var messageType = messageInterface.GetGenericArguments()[0];
-                
-                if (!handlersByMessageType.TryGetValue(messageType, out var handlers))
-                {
-                    handlers = new List<object>();
-                    handlersByMessageType[messageType] = handlers;
-                }
-                
-                // Resolve instance through service provider
-                handlers.Add(serviceProvider.GetRequiredService(handlerType));
+                handlers = new List<object>();
+                handlersByMessageType[messageType] = handlers;
             }
+            
+            handlers.Add(Activator.CreateInstance(handlerType)!);
         }
         
-        return handlersByMessageType.ToDictionary(kvp => kvp.Key, kvp => (IReadOnlyList<object>)kvp.Value);
+        _handlersByMessageType = handlersByMessageType.ToDictionary(kvp => kvp.Key, kvp => (IReadOnlyList<object>)kvp.Value);
     }
 
     public async Task HandleAsync<TMessage>(TMessage message, CancellationToken cancellationToken) 
         where TMessage : IMessage
     {
-        // Direct invocation - no runtime reflection
         if (_handlersByMessageType.TryGetValue(typeof(TMessage), out var handlers))
         {
             foreach (var handler in handlers)
             {
-                await ((IProjectionHandler<TMessage>)handler).HandleAsync(message, cancellationToken);
+                var handlerInterface = handler.GetType().GetInterfaces()
+                    .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IProjectionHandler<>));
+                
+                var method = handlerInterface.GetMethod(nameof(IProjectionHandler<IMessage>.HandleAsync));
+                await (Task)method!.Invoke(handler, new object[] { message, cancellationToken })!;
             }
         }
     }
